6f783e21ebc9e01aab9e549cb89bc361602a8587
Helder-Miambo <104924682+Helder-Miambo@users.noreply.github.com>
2022-05-04 18:39:48 +0100

    Commit inicial
    O primeiro commit do site

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+$('#menu').click(function(){
+ $(this).toggleclass('fa-times');
+ $('header').toggleclass('toggle');
+});
+
+$(window).on('scroll load',function(){
+
+    $('#menu').removeclass('fa-times');
+    $('header').toggleclass('toggle');
+});
+
+$(window).on('scroll load',function(){
+
+    $('#menu').removeclass('fa-times');
+    $('header').removeclass('toggle');
+
+    if($(window).scrolltop() > 0){
+        $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+});
+
+// smooth scrolling
+
+$('a[href*="#"]').on('click',function(e){ 
+
+
+  e.preventdefault();
+
+  $('html, body').animate({
+      scrolltop : $($(this).attr('href')).offset().top, 
+
+  
+},
+ 500,
+ 'linear'
+);
+
+
+});
+
+})
